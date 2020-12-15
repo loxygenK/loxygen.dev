7b0bbd95586902e0a1ff53ab677ea1a99485f4ed
loxygenK <loxygen.k@gmail.com>
2020-12-15 09:19:30 +0900

    🍱 Defined theme colors

---
+++ src/style/Common.ts
@@ -0,0 +1,8 @@
+export const MainBackground = "#e8ebed";
+export const AccentBackground = "#ccdaeb"
+export const MainTextColor = "#2d2d2e";
+export const PrimaryAccentColor = "#376dab";
+
+// TODO: Is this really ok?
+export const SecondaryAccentColor = "#40b890";
+
