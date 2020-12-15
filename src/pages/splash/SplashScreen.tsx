6ebd72e66e3079c4c81bffd9604dfef599e9f01e
loxygenK <loxygen.k@gmail.com>
2020-12-16 01:15:25 +0900

    🚨 Fixed warnings

--- src/pages/splash/SplashScreen.tsx
+++ src/pages/splash/SplashScreen.tsx
@@ -7,48 +7,50 @@ const message = "ssh visitor@flisan startx".replace(/ /g, "\u00a0");
 const charDelay = 0.05;
 
 const SplashscreenRoot = styled.div`
-  display: flex;
   position: fixed;
   top: 0;
   left: 0;
+  display: flex;
+  align-items: center;
+
+  justify-content: center;
 
   width: 100vw;
   height: 100vh;
-  color: ${Colors.WhiteTextColor};
-  background-color: ${Colors.BlackBackground};
 
   font-size: 2.3em;
   font-weight: 700;
+  color: ${Colors.WhiteTextColor};
+  background-color: ${Colors.BlackBackground};
 
-  justify-content: center;
-  align-items: center;
-
-  animation: 0.35s slideout ${charDelay * (message.length) + 0.5}s forwards;
+  animation: 0.35s slideout ${charDelay * message.length + 0.5}s forwards;
 
   @keyframes slideout {
-    from {top: 0}
-    to {top: -100vh; display: none;}
+    from {
+      top: 0;
+    }
+    to {
+      top: -100vh;
+      display: none;
+    }
   }
 `;
 
 const Splashscreen = () => {
-  
   const [index, setIndex] = React.useState(0);
 
   React.useEffect(() => {
     const timer = setInterval(() => {
-      setIndex(index => index + 1);
-    }, charDelay * 1000)
-    return () => clearInterval(timer)
-  }, [setIndex])
+      setIndex((index) => index + 1);
+    }, charDelay * 1000);
+    return () => clearInterval(timer);
+  }, [setIndex]);
 
   return (
     <SplashscreenRoot>
-      <TerminalStyle blinking>
-        {message.substring(0, index)}
-      </TerminalStyle>
+      <TerminalStyle blinking>{message.substring(0, index)}</TerminalStyle>
     </SplashscreenRoot>
-  )
-}
+  );
+};
 
 export default Splashscreen;
