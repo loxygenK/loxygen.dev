e17b0c3c7bd9774f0c638ce675a9f61d8cb41e42
loxygenK <loxygen.k@gmail.com>
2020-12-15 19:15:26 +0900

    ✨ Created footer

--- src/pages/common/Footer.tsx
+++ src/pages/common/Footer.tsx
@@ -1,8 +1,18 @@
-
 import * as React from "react"
+import * as Colors from "@app/style/Colors";
+import styled from "styled-components";
+
+const FooterRoot = styled.div`
+  background-color: ${Colors.AccentBackground};
+  padding: 2em;
+  text-align: center;
+`;
 
 export const Footer = () => (
-  <header>
-    <div>** Footer **</div>
-  </header>
+  <footer>
+    <FooterRoot>
+      <div><b>(c) 2020 Flisan ／ [visitor@flisan ~]</b></div>
+      <div>フライさんのブログです。</div>
+    </FooterRoot>
+  </footer>
 )
