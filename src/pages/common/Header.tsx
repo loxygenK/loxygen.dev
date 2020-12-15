1b43d71ea5a708e124a8d2391b756ba0799ab3b6
loxygenK <loxygen.k@gmail.com>
2020-12-15 13:14:17 +0900

    🎨 Styled header

--- src/pages/common/Header.tsx
+++ src/pages/common/Header.tsx
@@ -1,7 +1,27 @@
 import * as React from "react"
 
+import * as Colors from "@app/style/Colors"
+import styled from "styled-components"
+import TerminalStyle from "./TerminalStyle";
+
+const HeaderRoot = styled.div`
+  background-color: ${Colors.AccentBackground};
+  padding: 2em;
+  text-align: center;
+`;
+
+const HeaderTitle = styled.div`
+  font-size: 1.75em;
+  font-weight: 900;
+`;
+
 export const Header = () => (
   <header>
-    <div>** Header **</div>
+    <HeaderRoot>
+      <HeaderTitle>
+        <TerminalStyle text="[visitor@flisan ~]" blinking />
+      </HeaderTitle>
+      Inspect what Flisan looks like
+    </HeaderRoot>
   </header>
 )
