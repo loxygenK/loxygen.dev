c4e22c5a8001fdcb4a8a70b3b2339b7d722ebe86
loxygenK <loxygen.k@gmail.com>
2020-12-16 23:37:45 +0900

    ✨ Created Greeting section

---
+++ src/pages/elements/greeting/Greeting.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import styled from "styled-components";
+import {MyIcon} from "./MyIcon";
+import * as Colors from "@app/style/Colors";
+
+const GreetingRoot = styled.div`
+  text-align: center;
+`;
+
+const GreetingMessage = styled.div`
+  font-size: 1.25em;
+  font-weight: bold;
+  color: ${Colors.SubTextColor}
+`;
+
+const GreetingMessageList = [
+  "Hi! Thank you for visiting here!",
+  "Wow, thank you for visiting here!",
+  "Oh-wow, I'm glad to see you here!",
+  "A visitor? Unbelievable!",
+  "Hello, visitor! Take your time if you're free!",
+  "Beep-beep, login succeeded, X session started...",
+  "[flisan@flisan ~]$ greet visitor --with-thanks",
+]
+
+export const Gretting = () => (
+  <GreetingRoot>
+    <MyIcon /><br />
+    <GreetingMessage>
+      {GreetingMessageList[Math.floor(Math.random() * GreetingMessageList.length)]}
+    </GreetingMessage>
+  </GreetingRoot>
+)
