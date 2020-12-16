71566dc4b707813582127788f81b48337a4eb300
loxygenK <loxygen.k@gmail.com>
2020-12-16 23:35:36 +0900

    ✨ Created 'MyIcon' component

---
+++ src/pages/elements/greeting/MyIcon.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const IconImageFrame = styled.img`
+  border-radius: 9999em;
+  border: 3px solid #c8ccd0;
+`;
+
+function em2px(em: number): number {
+  return parseFloat(getComputedStyle(document.documentElement).fontSize) * em;
+}
+
+export const MyIcon = () => (
+  <IconImageFrame src={
+    `https://www.gravatar.com/avatar/81b5400c90be309ee83aa9d13a3d8cc5?s=${em2px(12.5)}`
+  } />
+);
