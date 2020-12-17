efeb6198c366269f8425ff4303b3f539b94eddce
loxygenK <loxygen.k@gmail.com>
2020-12-18 01:16:26 +0900

    🎨 Adjusted the disappearing animation of splash screen

--- src/pages/splash/SplashScreen.tsx
+++ src/pages/splash/SplashScreen.tsx
@@ -31,7 +31,7 @@ const SplashscreenRoot = styled.div`
   overflow-wrap: anywhere;
   background-color: ${Colors.BlackBackground};
 
-  animation: 0.35s slideout ${charDelay * message.length + 0.5}s forwards;
+  animation: 0.5s cubic-bezier(.14,.59,.4,.83) slideout ${charDelay * message.length + 0.5}s forwards;
 
   @keyframes slideout {
     from {
