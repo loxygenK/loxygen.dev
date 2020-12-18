f35b5f921cd883e9a44f62837617c4bb0436309d
loxygenK <loxygen.k@gmail.com>
2020-12-18 11:48:47 +0900

    🍻 Load data from remote in AppRoot

--- src/AppRoot.tsx
+++ src/AppRoot.tsx
@@ -5,7 +5,9 @@ import { Header } from "@app/pages/common/Header";
 import * as Colors from "@app/style/Colors";
 import Splashscreen from "./pages/splash/SplashScreen";
 import { Gretting } from "./pages/elements/greeting/Greeting";
-import { Heading } from "./pages/common/outline/Heading";
+import { Section } from "./pages/common/outline/Section";
+import { Introduction } from "./pages/elements/introduction/Introduction";
+import { DataSet, fetchFromDefaultDataSet } from "./types/datas/RemoteFetcher";
 
 const AppRootWrapper = styled.div`
   display: flex;
@@ -20,6 +22,10 @@ const Expand = styled.div`
   flex: 1;
 `;
 
+const Centerize = styled.div`
+  text-align: center;
+`;
+
 const AppContentWrapper = styled.div`
   max-width: 45em;
   padding: 1em;
@@ -27,16 +33,62 @@ const AppContentWrapper = styled.div`
   word-wrap: break-word;
 `;
 
-export const AppRoot = () => (
-  <AppRootWrapper>
-    <Header />
-    <Splashscreen />
-    <Expand>
-      <AppContentWrapper>
-        <Gretting />
-        <Heading anchor="fundamental" caption="自己紹介" />
-      </AppContentWrapper>
-    </Expand>
-    <Footer />
-  </AppRootWrapper>
-);
+type State = {
+  data?: Map<DataSet, any | null>;
+};
+
+export class AppRoot extends React.Component<Record<string, unknown>, State> {
+  constructor({}) {
+    super({});
+    this.state = {
+      data: undefined,
+    };
+  }
+
+  componentDidMount() {
+    Promise.all([
+      fetchFromDefaultDataSet("achievements"),
+      fetchFromDefaultDataSet("fundamental"),
+      fetchFromDefaultDataSet("technical"),
+    ]).then((value: [object | null, object | null, object | null]) => {
+      console.log("Fetched!");
+      this.setState({
+        data: new Map([
+          ["achievements", value[0]],
+          ["fundamental", value[1]],
+          ["technical", value[2]],
+        ]),
+      });
+    });
+  }
+
+  render() {
+    return (
+      <AppRootWrapper>
+        <Header />
+        {<Splashscreen />}
+        <Expand>
+          <AppContentWrapper>
+            <Gretting />
+            {this.buildContent()}
+          </AppContentWrapper>
+        </Expand>
+        <Footer />
+      </AppRootWrapper>
+    );
+  }
+
+  buildContent(): React.ReactNode {
+    console.log(this.state.data);
+    if(this.state.data == null) return (
+      <Centerize>This is taking unexpectedly long time...</Centerize>
+    );
+    console.log(this.state.data.get("fundamental"));
+    return (
+      <>
+        <Section caption="自己紹介" anchor="introduction">
+        </Section>
+      </>
+    )
+  }
+}
---
+++ src/pages/elements/introduction/Introduction.tsx
@@ -0,0 +1,8 @@
+import {Section} from "@app/pages/common/outline/Section";
+import * as React from "react";
+
+export const Introduction = () => (
+  <Section caption="自己紹介" anchor="introduction">
+    Hi, there!
+  </Section>
+)
