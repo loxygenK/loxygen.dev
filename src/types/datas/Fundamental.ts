cefa6bea8aafa07ff4bcaad3d46cfa5e7f01f7e7
loxygenK <loxygen.k@gmail.com>
2020-12-19 15:46:04 +0900

    🏷️ Defined types for the dataset

---
+++ src/types/datas/Fundamental.ts
@@ -0,0 +1,14 @@
+export type Fundamental = {
+  name: Abbreviation;
+  belong: Belonging;
+  description: string;
+}
+
+export type Belonging = {
+  school: {name: Abbreviation, class: Abbreviation}
+}
+
+export type Abbreviation = {
+  short: string;
+  long: string;
+}
