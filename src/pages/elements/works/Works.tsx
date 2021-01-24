38ab128825ca1af49c52dde75a15b03b011ce652
loxygenk <loxygen.k@gmail.com>
2021-01-24 21:30:14 +0900

    📱 Restricted grid column number to 1 when mobile

--- src/pages/elements/works/Works.tsx
+++ src/pages/elements/works/Works.tsx
@@ -4,15 +4,18 @@ import { Section } from "@app/pages/common/outline/Section";
 import { ProgramContainer } from "@app/pages/elements/works/ProgramContainer";
 
 import style from "@css/elements/program.module.scss";
+import { SubSection } from "@app/pages/common/outline/SubSection";
 
 export const Works = (props: { works: Program[] }) => {
   return (
     <Section name="works">
-      <div className={style.programList}>
-        {Object.entries(props.works).map(([name, value], index) => (
-          <ProgramContainer name={name} program={value} key={index} />
-        ))}
-      </div>
+      <SubSection name={"list --grid ~/works/*"}>
+        <div className={style.programList}>
+          {Object.entries(props.works).map(([name, value], index) => (
+            <ProgramContainer name={name} program={value} key={index} />
+          ))}
+        </div>
+      </SubSection>
     </Section>
   );
 };
